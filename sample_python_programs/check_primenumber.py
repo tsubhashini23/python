06a93ba9919f93e506293f2914af0c400d5cc77f
tsubhashini23 <tsubhashini23@gmail.com>
2025-06-20 18:31:57 +0530

    prime concept

---
+++ sample_python_programs/check_primenumber.py
@@ -0,0 +1,8 @@
+def check_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, n-1):
+        if n % i == 0:
+            return False  # Not a prime number
+    return True # Prime Number
+print(check_prime(6))
