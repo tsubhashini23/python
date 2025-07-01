d795f454a59a09389f4a8d71f753f6829ab7eaa0
tsubhashini23 <tsubhashini23@gmail.com>
2025-07-01 18:11:13 +0530

    reverse_ist & find_duplicates

--- sample_python_programs/find_duplicates.py
+++ sample_python_programs/find_duplicates.py
@@ -1,16 +1,19 @@
-# def find_duplicates(list):
-#     ln = len(list)
-#     duplicates = []
-#     # count = 0
-#     for i in range(ln):
-#         for j in range(i+1, ln):
-#             if list[i] == list[j] and list[i] not in duplicates:
-#                 duplicates.append(list[i])
-#     return duplicates
-                
-                
-# print(find_duplicates([1,2,3,4,5,5,5,7,9,7,6,6,8,8]))
+#Find duplicates in a number list
 
+def find_duplicates(list):
+    ln = len(list)
+    duplicates = []
+    # count = 0
+    for i in range(ln):
+        for j in range(i+1, ln):
+            if list[i] == list[j] and list[i] not in duplicates:
+                duplicates.append(list[i])
+    return duplicates
+                
+                
+print(find_duplicates([1,2,3,4,5,5,5,7,9,7,6,6,8,8]))
+
+#Find duplicates in a string
 
 def find_duplicate_letters(name):
     updated_name = name.lower()
---
+++ sample_python_programs/reverse_list.py
@@ -0,0 +1,45 @@
+#Reverse number list
+
+def reverse_list(list):
+        reversed_list = []
+        ln = len(list)
+        # print(ln)
+        for i in range(ln-1, -1, -1):            
+            if list[i] not in reversed_list:
+                reversed_list.append(list[i])
+        return reversed_list
+    
+print(reverse_list([30,2,3,4,5,6]))
+
+#Reverse using EXTENDED SLICING method: list[::-1] (Start from the end of the sequence and move backward by 1 step until the beginning.”)
+# start → not given → defaults to end of list if step is negative
+
+# stop → not given → goes until the beginning (index -1) when stepping backwards
+
+# step = -1 → this means "go backwards one element at a time"
+
+def play_with_list(list):
+    print("Sorted List:", sorted(list))
+    print("Reversed List",list[::-1])
+    
+play_with_list([5,1,7,2,3])
+
+#Reverse a string
+
+def reverse_string(name):
+    updated_name = name.lower()
+    new_list = list(updated_name)
+    ln = len(new_list)
+    reversed_list = []
+    for i in range(ln-1, -1, -1):
+        reversed_list.append(new_list[i])
+    return reversed_list
+
+print(reverse_string("dog"))
+
+
+
+
+    
+
+    
