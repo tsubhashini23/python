b810a0ce916913851aeefae52a78ea8367e4ad0c
tsubhashini23 <tsubhashini23@gmail.com>
2025-07-04 12:04:02 +0530

    Programs_july 4

--- sample_python_programs/infy_programs.py
+++ sample_python_programs/infy_programs.py
@@ -7,3 +7,39 @@ def reverse_string(name):
     return result
 
 print(reverse_string("python"))
+
+
+#Sum of all prime numbers
+def prime_number(n):
+    for i in range(2, n):
+        if n%i == 0:
+            return False #Not Prime
+    return True #Prime
+
+def find_sum_of_prime(num):
+    result = []
+    for i in range(2, num):
+        if (prime_number(i)):
+            result.append(i)
+    final = sum(result)
+    return final
+
+print(find_sum_of_prime(15))
+
+
+#Find all the consonants from a string
+
+def find_consonants(data):
+    new_data = data.lower()
+    result = []
+    vowels = "aeiouAEIOU"
+    for c in new_data:
+        if c not in vowels:
+            result.append(c)
+    final = print("".join(result))
+    return final
+
+find_consonants("subha")
+
+            
+    
