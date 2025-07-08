2ee7f5b146481f4092a598c411d43eb7dffee237
tsubhashini23 <tsubhashini23@gmail.com>
2025-07-08 17:36:22 +0530

    Print Patterns

---
+++ sample_python_programs/patterns.py
@@ -0,0 +1,29 @@
+
+def pattern_one(num):
+    print("Pattern One")
+    for i in range(num):
+        for j in range(num):
+            print("*", end=" ")
+        print("\n")      
+pattern_one(5)
+
+def pattern_two(num):
+    print("Pattern Two")
+    for i in range(num):
+        for j in range(i+1):
+            print("*", end=" ")
+        print("\n")    
+pattern_two(5)
+
+def pattern_three(num):
+    print("Pattern Three")
+    for i in range(num):
+        for j in range(num):
+            if j >= num-i-1:
+                print("*", end = " ")
+            else:
+                print(" ", end = " ")   
+        print("\n")    
+pattern_three(5)
+        
+        
