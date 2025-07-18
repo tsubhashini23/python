c87dff7b6dce55d89efeb0ba0e40f242b5aa06a5
tsubhashini23 <tsubhashini23@gmail.com>
2025-07-18 18:06:48 +0530

    selenium_sample

--- selenium_sample.py
+++ selenium_sample.py
@@ -1,17 +1,30 @@
 import time
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+#sample test with fixture:
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+    
 
-name = "Rahul"
-driver.find_element(By.CSS_SELECTOR, "#name").send_keys(name)
-driver.find_element(By.ID, "alertbtn").click()
-alert = driver.switch_to.alert
-alertText = alert.text
-print(alertText)
-assert name in alertText
-time.sleep(5)
-alert.accept()
-#alert.dismiss()
+def test_sample(driver):
+    driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+    name = "Rahul"
+    driver.find_element(By.CSS_SELECTOR, "#name").send_keys(name)
+    driver.find_element(By.ID, "alertbtn").click()
+    time.sleep(5)
+    alert = driver.switch_to.alert
+    alertText = alert.text
+    print(alertText)
+    assert name in alertText
+    alert.accept()
+    #alert.dismiss()
+
+
+
+    
