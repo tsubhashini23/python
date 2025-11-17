229c6a8931ae4567376a3b617ab68a256d3ecb06
tsubhashini23 <tsubhashini23@gmail.com>
2025-11-17 12:45:37 +0530

    Mask numbers using pattern

--- sample_python_programs/use_of_set.py
+++ sample_python_programs/use_of_set.py
@@ -8,31 +8,55 @@
 
 # Remove duplicates from a list
 
-numbers_list = [1,2,2,3,3,4,5,6,6]
-new_numbers_list = set(numbers_list)
-print(new_numbers_list)
+# numbers_list = [1,2,2,3,3,4,5,6,6]
+# new_numbers_list = set(numbers_list)
+# print(new_numbers_list)
+#
+#
+# #Use of join
+#
+# fruit_list = ['apple','banana','orange']
+# print(",".join(fruit_list))
+# print(type(",".join(fruit_list)))
+#
+#
+# # Set operations
+#
+# a = set([1,2,3])
+# b = set([3,4,5])
+#
+# print(a.union(b))
+# print(a.intersection(b))
+# print(a.difference(b))
+#
+# #Check whether a value is in the list or not
+#
+# my_set = set([10,20,30,30])
+# print(30 in my_set)
+# print(40 in my_set)
+#
+# #Convert string to unique characters
+# s = "subha"
+# my_char_set = set(s)
+# new_set = dict.fromkeys(s)
+# print(my_char_set)
+# print("New set as is:",new_set)
+# print("Only the keys",new_set.keys())
+# print("".join(new_set.keys()))
+# print("New set as list",list(new_set))
 
-# Set operations
+# Mask numbers using pattern
+num = "1234-5678-9123-4455"
+num_list = list(num)
+print(num_list)
+ln = len(num_list)
+print(ln)
 
-a = set([1,2,3])
-b = set([3,4,5])
+numbers = ["0","1","2","3","4","5","6","7","8","9"]
 
-print(a.union(b))
-print(a.intersection(b))
-print(a.difference(b))
+for i in range(0, ln-4):
+    if  num_list[i] in numbers:
+        num_list[i] = "*"
+print("".join(num_list))
 
-#Check whether a value is in the list or not
 
-my_set = set([10,20,30,30])
-print(30 in my_set)
-print(40 in my_set)
-
-#Convert string to unique characters
-s = "subha"
-my_char_set = set(s)
-new_set = dict.fromkeys(s)
-print(my_char_set)
-print("New set as is:",new_set)
-print("Only the keys",new_set.keys())
-print("".join(new_set.keys()))
-print("New set as list",list(new_set))
